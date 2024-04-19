488bd8282e34587b7d0ebb62045913c3c69b6b8d
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-04-19 17:56:31 +0100

    backend added steamdownload and steam run

--- ArkServer/server/urls.py
+++ ArkServer/server/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('download_steamcmd', views.download_steamcmd, name="download_steamcmd"),
+    path('run_steamcmd', views.run_steamcmd, name="run_steamcmd"),    
 ]
