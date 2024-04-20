b12b1e994af6018e86c0ab53c036599a2ff0c9d3
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-04-20 22:57:27 +0100

    update

--- ArkServer/server/views.py
+++ ArkServer/server/views.py
@@ -15,7 +15,7 @@ class StartArkServer(APIView):
 
         process = subprocess.Popen([steamcmd_path], cwd=steamcmd_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        def read_output_until_line_contains(target_line, prompt, timeout=30):
+        def read_output_until_prompt(prompt, timeout=30):
             start_time = time.time()
             while True:
                 if process.poll() is not None:
@@ -25,36 +25,32 @@ class StartArkServer(APIView):
                 output_line = process.stdout.readline().strip()
                 if output_line:
                     logging.info(f"steamcmd output: {output_line}")
-                    if target_line in output_line or prompt in output_line:
-                        logging.info(f"Received target line or prompt: {target_line} or {prompt}")
+                    if prompt in output_line:
+                        logging.info(f"Received prompt: {prompt}")
                         return True
                 
                 if time.time() - start_time > timeout:
-                    logging.warning(f"Timeout reached while waiting for '{target_line}' or '{prompt}'")
+                    logging.warning(f"Timeout reached while waiting for '{prompt}'")
                     logging.info(f"Current output buffer: {process.stdout.read().strip()}")
                     return False
 
             return False
 
         commands = [
-            ("Steam>", "", 60),  # Waiting for Steam> prompt after launch
             ("", "login anonymous", 30),
-            ("Steam>", "", 30),  # Waiting for Steam> prompt after login
-            ("", "app_update 2430930 validate", 600),
-            ("Steam>", "", 30),  # Waiting for Steam> prompt after app_update
-            ("", "quit", 30),
+            ("Steam>", "app_update 2430930 validate", 600),
+            ("Steam>", "quit", 30),
         ]
 
-        for target_line, cmd, timeout in commands:
-            logging.info(f"Waiting for: {target_line}")
-            if target_line:
-                if not read_output_until_line_contains(target_line, target_line, timeout):
-                    logging.warning(f"Failed to receive '{target_line}' before sending {cmd.strip()} command")
-                    break
-
+        for prompt, cmd, timeout in commands:
             logging.info(f"Sending command: {cmd.strip()}")
             process.stdin.write(f"{cmd}\n")
             process.stdin.flush()
+
+            logging.info(f"Waiting for prompt: {prompt}")
+            if not read_output_until_prompt(prompt, timeout):
+                logging.warning(f"Failed to receive '{prompt}' before sending {cmd.strip()} command")
+                break
 
         process.terminate()
 
