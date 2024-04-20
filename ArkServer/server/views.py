1c3a92ea56b0d34a9ddabeb1941aaff0afc44b26
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-04-20 23:20:33 +0100

    update

--- ArkServer/server/views.py
+++ ArkServer/server/views.py
@@ -1,9 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import subprocess
+import pexpect
 import logging
-import time
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,38 +12,19 @@ class StartArkServer(APIView):
         steamcmd_dir = 'steamcmd'  # Update with your actual path
         steamcmd_path = f"{steamcmd_dir}/steamcmd.exe"
 
-        process = subprocess.Popen([steamcmd_path], cwd=steamcmd_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+        child = pexpect.spawn(steamcmd_path)
 
-        def read_output(process, target_line, timeout=30):
-            start_time = time.time()
-            while True:
-                line = process.stdout.readline().strip()
-                if line:
-                    logging.info(f"steamcmd output: {line}")
-                    if target_line in line:
-                        logging.info(f"Received target line: {target_line}")
-                        return True
-                
-                if time.time() - start_time > timeout:
-                    logging.warning(f"Timeout reached while waiting for '{target_line}'")
-                    logging.info(f"Current output buffer: {process.stdout.read().strip()}")
-                    return False
+        def send_command(cmd):
+            child.sendline(cmd)
+            child.expect('Steam>')
 
-        commands = [
-            ("Loading Steam API...OK", "login anonymous\n"),
-            ("Waiting for user info...OK", "app_update 2430930 validate\n"),
-            ("Success! App '2430930' fully installed.", "quit\n"),
-        ]
+        send_command("login anonymous")
 
-        for target_line, cmd in commands:
-            logging.info(f"Waiting for: {target_line}")
-            if read_output(process, target_line):
-                logging.info(f"Sending command: {cmd.strip()}")
-                process.stdin.write(cmd)
-                process.stdin.flush()
-                time.sleep(1)  # Adding a 1-second delay
-                
-        process.terminate()
+        # Continue with other commands
+        send_command("app_update 2430930 validate")
+        send_command("quit")
+
+        child.close()
 
     def post(self, request, *args, **kwargs):
         try:
