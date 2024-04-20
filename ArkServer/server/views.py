3e2fd66c2fa7b74e125f912e9439d2910b3fbd89
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-04-20 23:13:29 +0100

    update

--- ArkServer/server/views.py
+++ ArkServer/server/views.py
@@ -15,7 +15,7 @@ class StartArkServer(APIView):
 
         process = subprocess.Popen([steamcmd_path], cwd=steamcmd_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        def read_output_until_line_contains(target_line, timeout=30):
+        def read_all_output(timeout=30):
             start_time = time.time()
             while True:
                 if process.poll() is not None:
@@ -25,37 +25,22 @@ class StartArkServer(APIView):
                 output_line = process.stdout.readline().strip()
                 if output_line:
                     logging.info(f"steamcmd output: {output_line}")
-                    if target_line in output_line:
-                        logging.info(f"Received target line: {target_line}")
-                        return True
                 
                 if time.time() - start_time > timeout:
-                    logging.warning(f"Timeout reached while waiting for '{target_line}'")
-                    logging.info(f"Current output buffer: {process.stdout.read().strip()}")
-                    return False
-
-            return False
+                    logging.warning("Timeout reached while waiting for output")
+                    break
 
         commands = [
-            ("Loading Steam API...OK", "login anonymous", 30),
-            ("Waiting for user info...OK", "app_update 2430930 validate", 600),
-            ("Success! App '2430930' fully installed.", "quit", 30),
+            "login anonymous",
+            "app_update 2430930 validate",
+            "quit"
         ]
 
-        for target_line, cmd, timeout in commands:
-            logging.info(f"Waiting for: {target_line}")
-            if read_output_until_line_contains(target_line, timeout):
-                logging.info(f"Sending command: {cmd.strip()}")
-                process.stdin.write(f"quit\n")
-                process.stdin.flush()
-                
-                # Add a delay after sending "login anonymous"
-                if cmd == "login anonymous":
-                    time.sleep(5)  # Wait for 5 seconds
-
-            else:
-                logging.warning(f"Failed to receive '{target_line}' output before sending {cmd.strip()} command")
-                break
+        for cmd in commands:
+            logging.info(f"Sending command: {cmd}")
+            process.stdin.write(f"{cmd}\n")
+            process.stdin.flush()
+            read_all_output()
 
         process.terminate()
 
