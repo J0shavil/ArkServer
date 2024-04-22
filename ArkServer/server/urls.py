6f12ed55e9bc0ef789d975f198431061626957d2
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-04-22 18:44:14 +0100

    update

--- ArkServer/server/urls.py
+++ ArkServer/server/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import StartArkServer
+from .views import StartArkServer, createserverstartup_bat
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('start_ark_server', StartArkServer.as_view(), name="start_ark_server"),
+    path('createbat', createserverstartup_bat, name="cratebat"),
        
 ]
