9e684e4eabdecb2f868307dff2933e16d83a02d5
J0shavil <142731625+J0shavil@users.noreply.github.com>
2024-05-01 12:03:35 +0100

    update

--- ArkServer/server/urls.py
+++ ArkServer/server/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import StartArkServer, createserverstartup_bat, login
+from .views import StartArkServer, createserverstartup_bat, login, register
 from .views import get_csrf_token
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('start_ark_server', StartArkServer.as_view(), name="start_ark_server"),
     path('createbat', createserverstartup_bat, name="createbat"),
     path('get_csrf_token/', get_csrf_token, name='get_csrf_token'),
-    path('login', login, name='login')
+    path('login', login, name='login'),
+    path('register', register, name='register'),
        
 ]
